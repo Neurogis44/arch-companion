5f44dcb0379fe4b48525a2c53e8688e6eb395dbb
Denis Desbiens <ddesbiens.42@gmail.com>
2026-07-22 13:12:04 -0400

    feat: initialize Arch Companion project

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Arch Companion
+Main entry point
+
+Author: Denis (Neurogis44)
+License: MIT
+"""
+
+def main():
+    print("Welcome to Arch Companion!")
+
+
+if __name__ == "__main__":
+    main()
